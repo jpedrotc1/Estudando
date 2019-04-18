169fba7f68be4cfbbc74b921efd1c591edaf71b2
jpedrotc1 <joao.costa@ccc.ufcg.edu.br>
2019-04-17 23:46:59 -0300

    primeira parte

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const app = express()
+const personRoute = require('./routes/person') 
+const path = require('path')
+
+
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} ==> ${req.originalUrl}`)
+
+    next()
+})
+
+app.use(personRoute)
+app.use(express.static('public'))
+
+//Handeler for 404
+app.use((req, res, next) => {
+    res.status(404).send('Deu aguia man')
+
+})
+//Handeler for 500
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.sendFile(path.join(__dirname, '../public/500.html'))
+})
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.info(`Server runnning on ${PORT}`))
+
---
+++ src/routes/person.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/person', (req, res) => {
+    if(req.query.name) {
+        res.send(`You have requested a person ${req.query.name}`)
+    }
+    else {
+    res.send('You have requested a person')
+    }
+})
+
+router.get('/person/:name', (req, res) => {
+    res.send(`You have requested a person ${req.params.name}`)
+})
+
+router.get('/error', (req, res) => {
+    throw new Error('Um erro forçado')
+})
+
+
+
+
+module.exports = router
+
+
