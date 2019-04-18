8e72d18a82dfe876194b5973ac1bff5550dd3e90
jpedrotc1 <joao.costa@ccc.ufcg.edu.br>
2019-04-18 00:08:58 -0300

    parte dois

--- src/models/costumer.model.js
+++ src/models/costumer.model.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose')
+
+const server = "mongodb+srv://dbuser:db123@cluster0-nburh.mongodb.net/test?retryWrites=true"
+const user = "dbuser"
+const password = "db123"
+
+mongoose.connect('mongodb+srv://dbuser:db123@cluster0-nburh.mongodb.net/test?retryWrites=true')
+
+const CostumerSchema = new mongoose.Schema({
+    name: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+module.exports = mongoose.model('Costumer', CostumerSchema)
---
+++ src/routes/costumer.js
@@ -0,0 +1,9 @@
+const CostumerModel = require('../models/costumer.model')
+const express = require('express')
+const router = express.Router()
+
+//Criando um novo costumer
+router.post('/costumer', (req, res) => {
+    
+})
+
